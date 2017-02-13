15d2c97ff0a9cf48514faf4521fec923dbb18c4b
Kamil Łoń <kamil-lon@wp.pl>
2017-02-13 20:42:49 +0100

    1.0.3
    added gra.php file

---
+++ gra.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+    
+<head>
+    <title>ToCzyTo?</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta name="description" content="Sprawdź swoją wiedzę, baw się, ucz się, rozwijaj się, rywalizuj i pomóż tworzyć serwis!">
+    <meta name="keywords" content="gra,game,quiz,zagadki,łamigłówki,logiczne,krzyżówki,pytania,wiedza,rozum,iq">
+    <meta name="author" content="Corleone">
+    <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cousine:700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bahiana" rel="stylesheet">
+</head>
+
+<body>
+    <div id="main">
+        <div id="header">
+            To czy To?
+        </div>
+        <div id="menu">
+        <div class="option">Graj!</div>
+        <a href="index.html#jakgrac"><div class="option">Jak grać?</div></a>
+        <a href="index.html#oprojekcie"><div class="option">O projekcie</div></a>
+        <a href="index.html#dodajpytanie"><div class="option">Dodaj pytanie!</div></a>
+        <a href="index.html#kontakt"><div class="option">Kontakt</div></a>
+        <div style="clear: both;"></div>
+        </div>
+        <div id="content">
+            <div id="punkty">0</div>
+            <div id="zegar">08:00</div>
+            <div id="zycia">♥♥♥</div>
+            <div style="clear:both;"></div>
+            <div id="pytanie">Kto prowadzi teleturniej "Jeden z dziesięciu"?</div>
+            <div class="odp">Tadeusz Sznuk</div>
+            <div id="toczyto">czy</div>
+            <div class="odp">Krzysztof Ibisz</div>
+            <div style="clear: both;"></div>
+        </div>
+        <div id="footer">
+            Wszelkie prawa zastrzeżone &copy 2017
+        </div>
+    </div>
+</body>
+</html>
