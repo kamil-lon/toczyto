5ae517034d10c8bcea45a3264023b2335af7c088
Kamil Łoń <kamil-lon@wp.pl>
2017-02-14 14:03:54 +0100

    1.0.4
    Add answers checking.

--- gra.php
+++ gra.php
@@ -1,3 +1,102 @@
+<?php
+
+try
+{
+    $polaczenie = new mysqli('localhost','root','','toczyto');
+    $polaczenie->query('SET NAMES utf8');
+    $polaczenie->set_charset('UTF-8');
+    if($polaczenie->connect_errno!=0)
+    {
+        throw new Expection(mysqli_connect_errno());
+    }
+    else
+    {
+
+            $zapytanie = $polaczenie->query("SELECT * FROM pytania");
+            $ile_pytan = $zapytanie->num_rows;
+                    $nr=rand(1,$ile_pytan);
+            $zapytanie = $polaczenie->query("SELECT * FROM pytania WHERE idpytania=$nr");
+        if($zapytanie->num_rows<1)
+        {
+            while($zapytanie->num_rows<1)
+            {
+                $nr+=1;
+                $zapytanie = $polaczenie->query("SELECT * FROM pytania WHERE idpytania=$nr");
+            }
+        }
+            $rezultat = $zapytanie->fetch_assoc();
+            $tresc = $rezultat['tresc'];
+            $odpa = $rezultat['odpa'];
+            $odpb = $rezultat['odpb'];
+            $odppop = $rezultat['odppop'];
+    }
+  
+}
+
+catch(Expection $ex)
+{
+    echo "ERROR ".$ex;
+}
+    
+
+if(isset($_POST['a']))
+{
+    $odpowiedz = $_POST['a'];
+    try
+    {
+        $polaczenie = new mysqli('localhost','root','','toczyto');
+        $polaczenie->query('SET NAMES utf8');
+        $polaczenie->set_charset('UTF-8');
+        if($polaczenie->connect_errno!=0)
+        {
+            throw new Expection(mysqli_connect_errno());
+        }
+        else
+        {
+            $odppop = $polaczenie->query("SELECT odppop FROM pytania WHERE odpa='$odpowiedz'");
+            $rezultat = $odppop->fetch_assoc();
+            $odppop = $rezultat['odppop'];
+            if($_POST['a']==$odppop)
+                echo "Dobrze";
+            else echo "Źle";
+            unset($_POST['a']);
+        }
+    }
+    catch(Expection $ex)
+    {
+        echo "ERROR ".$ex;
+    }
+}
+
+if(isset($_POST['b']))
+{
+    $odpowiedz = $_POST['b'];
+    try
+    {
+        $polaczenie = new mysqli('localhost','root','','toczyto');
+        $polaczenie->query('SET NAMES utf8');
+        $polaczenie->set_charset('UTF-8');
+        if($polaczenie->connect_errno!=0)
+        {
+            throw new Expection(mysqli_connect_errno());
+        }
+        else
+        {
+            $odppop = $polaczenie->query("SELECT odppop FROM pytania WHERE odpb='$odpowiedz'");
+            $rezultat = $odppop->fetch_assoc();
+            $odppop = $rezultat['odppop'];
+            if($_POST['b']==$odppop)
+                echo "Dobrze";
+            else echo "Źle";
+            unset($_POST['b']);
+        }
+    }
+    catch(Expection $ex)
+    {
+        echo "ERROR ".$ex;
+    }
+}
+?>
 <!DOCTYPE HTML>
 <html lang="pl">
     
@@ -27,14 +126,16 @@
         <div style="clear: both;"></div>
         </div>
         <div id="content">
-            <div id="punkty">0</div>
-            <div id="zegar">08:00</div>
-            <div id="zycia">♥♥♥</div>
+            <div class="statystyki">0</div>
+            <div class="statystyki">08:00</div>
+            <div class="statystyki" style="color: firebrick;">♥♥♥</div>
             <div style="clear:both;"></div>
-            <div id="pytanie">Kto prowadzi teleturniej "Jeden z dziesięciu"?</div>
-            <div class="odp">Tadeusz Sznuk</div>
-            <div id="toczyto">czy</div>
-            <div class="odp">Krzysztof Ibisz</div>
+            <div id="pytanie"><?php echo $tresc; ?></div>
+        <form action="gra.php" method="post">
+            <input type="submit" name="a" class="odp" value="<?php echo $odpa; ?>">
+            <div id="czy">czy</div>
+            <input type="submit" name="b" class="odp" value="<?php echo $odpb; ?>">
+        </form>
             <div style="clear: both;"></div>
         </div>
         <div id="footer">
