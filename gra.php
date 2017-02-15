e238e1da7370b05dc2f5f501f4ab01a98e6ba08b
Kamil Łoń <kamil-lon@wp.pl>
2017-02-15 16:54:32 +0100

    1.0.5
    added good and bad answer and end-questions reactions, it works

--- gra.php
+++ gra.php
@@ -1,100 +1,20 @@
 <?php
+session_start();
 
-try
+if(!isset($_SESSION['nr']))
 {
-    $polaczenie = new mysqli('localhost','root','','toczyto');
-    $polaczenie->query('SET NAMES utf8');
-    $polaczenie->set_charset('UTF-8');
-    if($polaczenie->connect_errno!=0)
-    {
-        throw new Expection(mysqli_connect_errno());
-    }
-    else
-    {
-
-            $zapytanie = $polaczenie->query("SELECT * FROM pytania");
-            $ile_pytan = $zapytanie->num_rows;
-                    $nr=rand(1,$ile_pytan);
-            $zapytanie = $polaczenie->query("SELECT * FROM pytania WHERE idpytania=$nr");
-        if($zapytanie->num_rows<1)
-        {
-            while($zapytanie->num_rows<1)
-            {
-                $nr+=1;
-                $zapytanie = $polaczenie->query("SELECT * FROM pytania WHERE idpytania=$nr");
-            }
-        }
-            $rezultat = $zapytanie->fetch_assoc();
-            $tresc = $rezultat['tresc'];
-            $odpa = $rezultat['odpa'];
-            $odpb = $rezultat['odpb'];
-            $odppop = $rezultat['odppop'];
-    }
-  
+   $_SESSION['pierwsze'] = true;
+   header ("Location: sprawdz_i_wylosuj_nowy_nr.php");
 }
-
-catch(Expection $ex)
+else
 {
-    echo "ERROR ".$ex;
-}
-    
+    $nr = $_SESSION['nr'];
+    $tresc = $_SESSION['tresc'];
+    $odpa = $_SESSION['odpa'];
+    $odpb = $_SESSION['odpb'];
+    $odppop = $_SESSION['odppop'];
 
-if(isset($_POST['a']))
-{
-    $odpowiedz = $_POST['a'];
-    try
-    {
-        $polaczenie = new mysqli('localhost','root','','toczyto');
-        $polaczenie->query('SET NAMES utf8');
-        $polaczenie->set_charset('UTF-8');
-        if($polaczenie->connect_errno!=0)
-        {
-            throw new Expection(mysqli_connect_errno());
-        }
-        else
-        {
-            $odppop = $polaczenie->query("SELECT odppop FROM pytania WHERE odpa='$odpowiedz'");
-            $rezultat = $odppop->fetch_assoc();
-            $odppop = $rezultat['odppop'];
-            if($_POST['a']==$odppop)
-                echo "Dobrze";
-            else echo "Źle";
-            unset($_POST['a']);
-        }
-    }
-    catch(Expection $ex)
-    {
-        echo "ERROR ".$ex;
-    }
-}
-
-if(isset($_POST['b']))
-{
-    $odpowiedz = $_POST['b'];
-    try
-    {
-        $polaczenie = new mysqli('localhost','root','','toczyto');
-        $polaczenie->query('SET NAMES utf8');
-        $polaczenie->set_charset('UTF-8');
-        if($polaczenie->connect_errno!=0)
-        {
-            throw new Expection(mysqli_connect_errno());
-        }
-        else
-        {
-            $odppop = $polaczenie->query("SELECT odppop FROM pytania WHERE odpb='$odpowiedz'");
-            $rezultat = $odppop->fetch_assoc();
-            $odppop = $rezultat['odppop'];
-            if($_POST['b']==$odppop)
-                echo "Dobrze";
-            else echo "Źle";
-            unset($_POST['b']);
-        }
-    }
-    catch(Expection $ex)
-    {
-        echo "ERROR ".$ex;
-    }
+    $punkty = $_SESSION['punkty'];
 }
 ?>
 <!DOCTYPE HTML>
@@ -118,7 +38,7 @@ if(isset($_POST['b']))
             To czy To?
         </div>
         <div id="menu">
-        <div class="option">Graj!</div>
+        <a href="gra.php"><div class="option">Graj!</div></a>
         <a href="index.html#jakgrac"><div class="option">Jak grać?</div></a>
         <a href="index.html#oprojekcie"><div class="option">O projekcie</div></a>
         <a href="index.html#dodajpytanie"><div class="option">Dodaj pytanie!</div></a>
@@ -126,15 +46,33 @@ if(isset($_POST['b']))
         <div style="clear: both;"></div>
         </div>
         <div id="content">
-            <div class="statystyki">0</div>
+            <div class="statystyki"><?php echo $punkty; ?></div>
             <div class="statystyki">08:00</div>
             <div class="statystyki" style="color: firebrick;">♥♥♥</div>
             <div style="clear:both;"></div>
-            <div id="pytanie"><?php echo $tresc; ?></div>
-        <form action="gra.php" method="post">
-            <input type="submit" name="a" class="odp" value="<?php echo $odpa; ?>">
+            <div id="pytanie" <?php
+                    if(isset($_SESSION['graj'])) echo 'style="border: 3px solid red;"';
+                                ?>><?php echo $tresc; ?>
+            </div>
+        <form action="sprawdz_i_wylosuj_nowy_nr.php" method="post">
+            <input type="submit" name="a" class="odp" 
+                <?php if(isset($_SESSION['graj'])) echo 'style="border: 2px solid red;"'; ?> 
+                   value="<?php 
+                          if(isset($_SESSION['graj']))
+                          {
+                            $odpa = $_SESSION['graj'];
+                            unset($_SESSION['graj']);
+                          }echo $odpa; ?>"
+            >
             <div id="czy">czy</div>
-            <input type="submit" name="b" class="odp" value="<?php echo $odpb; ?>">
+            <input type="submit" name="b" class="odp"
+            <?php if(isset($_SESSION['dodaj'])) echo 'style="border: 3px solid red;"'; ?> 
+            value="<?php
+                   if(isset($_SESSION['dodaj']))
+                   {
+                       $odpb = $_SESSION['dodaj'];
+                       unset($_SESSION['dodaj']);
+                   }echo $odpb; ?>">
         </form>
             <div style="clear: both;"></div>
         </div>
@@ -144,3 +82,12 @@ if(isset($_POST['b']))
     </div>
 </body>
 </html>
+<?php
+
+unset($_SESSION['nr']);
+unset($_SESSION['tresc']);
+unset($_SESSION['odpa']);
+unset($_SESSION['odpb']);
+unset($_SESSION['odppop']);
+
+?>
